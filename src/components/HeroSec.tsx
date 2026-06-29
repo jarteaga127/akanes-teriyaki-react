4d1c8fb775e16df353585fba5ac2586bb294ea2c
Joseph Arteaga <jarteaga127@gmail.com>
2026-06-29 12:41:18 +0900

    Created home page and hero section

---
+++ src/components/HeroSec.tsx
@@ -0,0 +1,12 @@
+const HeroSec = () => {
+    return ( 
+        <section>
+            <div className="hero-text">
+                <h1>Welcome to Akane's Teriyaki Grill</h1>
+                <button>Check out our menu.</button>
+            </div>
+        </section>
+     );
+}
+ 
+export default HeroSec;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import HeroSec from "../components/HeroSec";
+
+const HomePage = () => {
+    return ( 
+        <>
+        <HeroSec/>
+        </>
+     );
+}
+ 
+export default HomePage;
