a9800cf69ebd7c5c74922916939b4b165270a754
Joseph Arteaga <jarteaga127@gmail.com>
2026-06-30 12:44:40 +0900

    Created Navbar and edited styles for Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,20 @@
+import "../styles/nav-bar.css"
+
 const Navbar = () => {
     return ( 
-        <nav></nav>
+        <nav>
+            <div className="site-logo"><header>Akane's Teriyaki</header></div>
+            <div className="nav-link-group">
+            <ul>
+                <li>Home</li>
+                <li>Our Menu</li>
+                <li>Book a table</li>
+                <li>Order online</li>
+                <li>About us</li>
+                <li>Contact us</li>
+            </ul>
+            </div>
+        </nav>
      );
 }
  
