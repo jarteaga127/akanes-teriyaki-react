4961fcf72f4287f1f179dece694c5bf8cb9b4812
Joseph Arteaga <jarteaga127@gmail.com>
2026-07-18 11:20:27 +0900

    Created menu page and menu card

---
+++ src/components/MenuCard.tsx
@@ -0,0 +1,13 @@
+const MenuCard = () => {
+    return ( 
+        <div className="menu-card">
+            <div className="menu-card-img"></div>
+            <div className="menu-card-text">
+                <div className="item-name">Teriyaki Chicken Bowl</div>
+                <div className="item-price">￥499</div>
+            </div>
+        </div>
+     );
+}
+ 
+export default MenuCard;
--- src/pages/OurMenu.tsx
+++ src/pages/OurMenu.tsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
+import MenuCard from "../components/MenuCard";
+import "../styles/our-menu.css"
 
 const OurMenu = () => {
     return ( 
         <section>
-            <h1>Nobody home!</h1>
+            <h1>Our Menu</h1>
+            <article>
+                <h2>Bowls</h2>
+                <div className="menu-grid">
+                    <MenuCard/>
+                    <MenuCard/>
+                    <MenuCard/>
+                    <MenuCard/>
+                    <MenuCard/>
+                    <MenuCard/>
+                </div>
+            </article>
             <Link to="/" className="link-001">Back to home</Link>
         </section>
      );
