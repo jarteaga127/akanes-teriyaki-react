7e1ac26be01cbcbc8ed1d437e1131ba92175a6d3
Joseph Arteaga <jarteaga127@gmail.com>
2026-08-04 12:28:40 +0900

    Made step 1 of the booking form into a separate component

--- src/components/Step1Details.tsx
+++ src/components/Step1Details.tsx
@@ -1,4 +1,5 @@
 import type { Step1Props } from "../types";
+import { TIME_SLOTS, SEAT_TYPES } from "../data/BookingData";
 
 const Step1Details: React.FC<Step1Props> = ({formData, updateFields, onNext}) => {
     return ( 
@@ -9,18 +10,18 @@ const Step1Details: React.FC<Step1Props> = ({formData, updateFields, onNext}) =>
                     <label htmlFor="time">What time will you be coming?</label>
                     <select name="time" id="time" value={formData.time} onChange={e => updateFields({time: e.target.value})} required disabled={!formData.date}>
                         <option>{formData.date ? "Select a time" : "Please tell us what day you are coming first, please."}</option>
-                        {timeSlots.map((slot) => (
+                        {TIME_SLOTS.map((slot) => (
                             <option key={slot.time} value={slot.time} disabled={!slot.isAvailable}>
                                 {slot.label}
                             </option>
                         ))}
                     </select>
                     <label htmlFor="guests">How many guests are you bringing?</label>
-                    <input type="number" name="guests" value={formData.guests} onChange={e => updateFields({guests: e.target.value})} min={0} max={10} />
+                    <input type="number" name="guests" value={formData.guests} onChange={e => updateFields({guests: parseInt(e.target.value, 10) || 1})} min={0} max={10} />
                     <label htmlFor="seatType">What kind of seat do you want?</label>
                     <select name="seatType" id="seatType" value={formData.seat} onChange={e=> updateFields({seat: e.target.value})} required >
                         <option>{formData.guests ? "Pick a seat" : "Please tell us how many people you are bringing first."} </option>
-                        {seatTypes.map((type) => (
+                        {SEAT_TYPES.map((type) => (
                             <option key={type.seat} value={type.seat} disabled={!type.isAvailable}>{type.label}</option>
                         ))}
                     </select>
